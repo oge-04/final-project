7b86fcdacb97d9f7053c6c60e6e63e45e19fa177
oge-04 <183093672+oge-04@users.noreply.github.com>
2024-11-24 19:27:25 -0500

    update1

---
+++ finalproject/static/login.js
@@ -0,0 +1,9 @@
+let email = "admin@admin.com"
+let password = "admin123"
+
+function login() {
+    let loginEmail = document.getElementById("loginEmail").value
+    let loginPassword = document.getElementById("loginPassword").value
+
+    
+}
