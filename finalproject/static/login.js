bf245fd14cbe7fa66731163e547c689019e7f214
oge-04 <183093672+oge-04@users.noreply.github.com>
2024-11-29 22:47:29 -0500

    commit

--- finalproject/static/login.js
+++ finalproject/static/login.js
@@ -4,9 +4,12 @@ function login() {
     let loginEmail = document.getElementById("loginEmail").value;
     let loginPassword = document.getElementById("loginPassword").value;
 
+    console.log("Entered Email: ", loginEmail);
+    console.log("Entered Password: ", loginPassword);
+
     if (loginEmail === email && loginPassword === password) {
         alert("Login Successful");
-        window.location ="products.html";
+        window.location.assign('./products.html');
     } else {
         alert("Incorrect Email or Password");
     }
